ce995f33c97371af865bc3ec7690f1ff15b2f287
Lely Sofa <lelysofaaa@gmail.com>
2024-03-16 17:57:05 +0700

    Menambah section Categories

--- App/Screens/HomeScreen/Categories.jsx
+++ App/Screens/HomeScreen/Categories.jsx
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../../Utils/GlobalApi'
+import Heading from '../../Components/Heading';
+import Colors from '../../Utils/Colors';
 
 export default function Categories() {
+
+    const [categories, setCategories]=useState([]);
+    useEffect(()=>{
+        getCategories();
+    },[])
+
+    const getCategories=()=>{
+        GlobalApi.getCategories().then(resp=>{
+            setCategories(resp?.categories);
+        })
+    }
   return (
-    <View>
-      <Text>Categories</Text>
+    <View style={{marginTop: 10}}>
+      <Heading text={'Categories'} isViewAll={true} />
+      <FlatList 
+      data={categories}
+      numColumns={4}
+      renderItem={({item, index})=>index<=3&&(
+        <View style={styles.container}>
+            <View style={styles.iconContainer}>
+                <Image source={{uri:item?.icon?.url}} style={{width: 32, height: 32}} />
+            </View>
+            <Text style={{fontFamily:'nunito-medium', marginTop: 5}}>
+                {item?.name}
+            </Text>
+        </View>
+      )}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    iconContainer:{
+        backgroundColor: Colors.lightBlue,
+        padding: 17,
+        borderRadius: 99
+    }
+})
